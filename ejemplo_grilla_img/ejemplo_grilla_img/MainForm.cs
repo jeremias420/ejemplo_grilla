e0d369083a162704e1ed1c769ccc966ac9eca583
jeremias420 <10002933+jeremias420@users.noreply.github.com>
2023-11-28 11:45:58 -0300

    comentarios

--- ejemplo_grilla_img/ejemplo_grilla_img/MainForm.cs
+++ ejemplo_grilla_img/ejemplo_grilla_img/MainForm.cs
@@ -41,7 +41,9 @@ namespace ejemplo_grilla_img
 					if(e.Value != null){
 						try
 						{
+      							//obtengo el path completo de la image fusionando el path de la aplicacion con el path de la imagen que tengo en la bd
 							string imgPath = Path.Combine(ApplicationPath, e.Value.ToString());
+       							//carga la imagen y la asigna a la celda
 							Image img = Image.FromFile(imgPath);
 							e.Value = img;
 						} catch {
@@ -52,10 +54,14 @@ namespace ejemplo_grilla_img
 		}
 		void MainFormLoad(object sender, EventArgs e)
 		{
+  			//esta configuracion evita que se autogeneren columnas, o sea que va a respetar las columas que crees manualmente en la grilla
 			dataGridView1.AutoGenerateColumns = false;
+   			//se obtiene el path de la aplicacion para usarlo luego para cargar las imagenes
 			ApplicationPath = AppDomain.CurrentDomain.BaseDirectory;
+   			//se crea instancia y conexion a la base de datos
 			cnx = new ClassConexionSQL();
 			cnx.CrearConexion();
+   			//se llena el grid con los datos de la tabla juegos
 			cnx.LLenarGrid(ref dataGridView1, "select * from juegos");
 		}
 	}
